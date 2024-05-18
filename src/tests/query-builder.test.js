ebf08b6d3120aea268dc387a1056bb8252c477d9
Greg <antonioimportant@gmail.com>
2024-05-18 16:10:41 -0300

    feat: add fabric in query-builder test

--- src/tests/query-builder.test.js
+++ src/tests/query-builder.test.js
@@ -2,20 +2,36 @@ import { describe, expect, it } from "vitest";
 import { Connection } from "../core/connection";
 import { QueryBuilder } from "../core/query-builder";
 
+const makeSut = () => {
+  const connectionProps = {
+    host: "fake_host",
+    user: "fake_user",
+    password: "fake_password",
+    database: "fake_database",
+  };
+  const queryBuilderProps = {
+    client: "fake_client",
+    connection: new Connection(connectionProps),
+  };
+  const queryBuilder = new QueryBuilder(queryBuilderProps);
+  return {
+    connectionProps,
+    queryBuilderProps,
+    queryBuilder,
+  };
+};
+
 describe("QueryBuilder Class", () => {
   it("should create a new query builder", () => {
-    const connectionProps = {
-      host: "fake_host",
-      user: "fake_user",
-      password: "fake_password",
-      database: "fake_database",
-    };
-    const queryBuilderProps = {
-      client: "fake_client",
-      connection: new Connection(connectionProps),
-    };
+    const { queryBuilder, connectionProps, queryBuilderProps } = makeSut();
 
-    const queryBuilder = new QueryBuilder(queryBuilderProps);
+    expect(queryBuilder).toBeInstanceOf(QueryBuilder);
+    expect(queryBuilder.connection.host).toBe(connectionProps.host);
+    expect(queryBuilder.client).toBe(queryBuilderProps.client);
+  });
+
+  it("should create a new query builder", () => {
+    const { queryBuilder, connectionProps, queryBuilderProps } = makeSut();
 
     expect(queryBuilder).toBeInstanceOf(QueryBuilder);
     expect(queryBuilder.connection.host).toBe(connectionProps.host);
