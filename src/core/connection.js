d3b26e916c60729030b747c615f6728e0f8affcd
Greg <antonioimportant@gmail.com>
2024-05-19 17:51:04 -0300

    feat: add urlConnection property in Connection class

--- src/core/connection.js
+++ src/core/connection.js
@@ -3,11 +3,13 @@ export class Connection {
   user;
   password;
   database;
+  urlConnection;
 
-  constructor({ host, user, password, database }) {
+  constructor({ host, user, password, database, urlConnection = undefined }) {
     this.database = database;
     this.user = user;
     this.password = password;
     this.host = host;
+    this.urlConnection = urlConnection;
   }
 }
