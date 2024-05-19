f0ad0511d229acf528eb88ea84973dbd6ae8786b
Greg <antonioimportant@gmail.com>
2024-05-19 20:56:04 -0300

    feat: add makeConnnection factory

--- src/tests/postgres-connection.test.js
+++ src/tests/postgres-connection.test.js
@@ -3,16 +3,24 @@ import { describe, expect, it } from "vitest";
 import { PostgresConnection } from "../core/postgres-connection";
 import "dotenv/config";
 
+const makeConnection = () => {
+  const connectionProps = {
+    host: process.env.DB_HOST,
+    password: process.env.DB_PASSWORD,
+    user: process.env.DB_USER,
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT,
+  };
+  const connection = new PostgresConnection(connectionProps);
+  return {
+    connection,
+    connectionProps,
+  };
+};
+
 describe("PostgreSQLConnection class", () => {
   it("should not throw when using sut.query", async () => {
-    const connectionProps = {
-      host: process.env.DB_HOST,
-      password: process.env.DB_PASSWORD,
-      user: process.env.DB_USER,
-      database: process.env.DB_DATABASE,
-      port: process.env.DB_PORT,
-    };
-    const connection = new PostgresConnection(connectionProps);
+    const { connection } = makeConnection();
     await connection.query("CREATE TABLE IF NOT EXISTS users");
 
     const sut = await connection.query("SELECT * FROM users");
